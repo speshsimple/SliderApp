beeeb01f93f1abce97776bd765da4f75d9cac172
Oleksandr Novikov <speshsimple@me.com>
2022-06-19 14:08:06 +0200

    new constraints. Add Stackviews

--- SliderApp/ViewController.swift
+++ SliderApp/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet weak var bigView: UIView!
+    
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+
     @IBOutlet weak var redTextField: UITextField!
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
